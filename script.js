b186c30ddac7c00c1d58cf49923686ccfaf6285e
rweirgithub <115409300+rweirgithub@users.noreply.github.com>
2023-05-22 20:50:36 +0200

    Cookie modal + Bootstrap node module update
    Added a bootstrap cookie modal with some custom JavaScript
    
    Updated the bootstrap node module

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// Cookie modal
+const cookieModal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+
+
+setTimeout(function () {
+    modal.style.display = 'flex'
+}, 1000)
+
+
+modalCloseBtn.addEventListener('click', function () {
+    modal.style.display = 'none'
+})
+
+
+// Carousel
+
